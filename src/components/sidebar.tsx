f4e37ef20f015c0551d535b175f85b302aa7b302
ayushpathak-48 <ayush010pathak@gmail.com>
2025-02-28 10:13:33 +0530

    logout functionality added

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,20 +5,30 @@ import { DottedSeparator } from "./DottedSeparator";
 import { Navigation } from "@/components/navigation";
 // import { WorkspaceSwitcher } from "./workspace-switcher";
 import { APP_TITLE } from "@/lib/constants";
+import { Button } from "./ui/button";
+import { signOutUser } from "@/actions/auth.action";
 // import Projects from "./projects";
 
 export const Sidebar = () => {
   return (
-    <aside className="h-full bg-neutral-100 p-4 w-full">
-      <Link href={"/"} className="text-3xl flex items-center justify-center">
-        {APP_TITLE}
-      </Link>
-      <DottedSeparator className="my-4" />
-      {/* <WorkspaceSwitcher />
-      <DottedSeparator className="my-4" /> */}
-      <Navigation />
-      <DottedSeparator className="my-4" />
-      {/* <Projects /> */}
+    <aside className="h-full bg-neutral-100 p-4 w-full flex flex-col justify-between">
+      <div>
+        <Link href={"/"} className="text-3xl flex items-center justify-center">
+          {APP_TITLE}
+        </Link>
+        <DottedSeparator className="my-4" />
+        <Navigation />
+      </div>
+      <div className="flex flex-col">
+        <DottedSeparator className="my-4" />
+        <Button
+          onClick={signOutUser}
+          variant={"outline"}
+          className="w-full mt-auto"
+        >
+          Logout{" "}
+        </Button>
+      </div>
     </aside>
   );
 };
