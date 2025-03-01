c1a3bfdd449753d5c9ba16cf867ddcaf051a7a38
ayushpathak-48 <ayush010pathak@gmail.com>
2025-03-01 18:13:18 +0530

    bug fix of children in admin layout page

--- src/app/(root)/(student)/layout.tsx
+++ src/app/(root)/(student)/layout.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-const layout = ({ children }: { children: PropsWithChildren }) => {
+const layout = ({ children }: PropsWithChildren) => {
   return <>{children}</>;
 };
 
