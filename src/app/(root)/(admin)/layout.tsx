d9250dad16b7f670fe2dd91ab03f86f174b82b36
ayushpathak-48 <ayush010pathak@gmail.com>
2025-03-01 18:11:49 +0530

    bug fix of children in admin layout page

--- src/app/(root)/(admin)/layout.tsx
+++ src/app/(root)/(admin)/layout.tsx
@@ -2,7 +2,7 @@ import { getAccount } from "@/actions/auth.action";
 import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
-const layout = async ({ children }: { children: PropsWithChildren }) => {
+const layout = async ({ children }: PropsWithChildren) => {
   const account = await getAccount();
   if (!account) {
     return redirect("/sign-in");
